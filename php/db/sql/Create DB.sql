12d4616a577442eda07c0e89e8b5c578d0ecd828
qodmsquf13 <qodmsquf13@naver.com>
2021-03-24 15:12:52 +0900

    Update css

---
+++ php/db/sql/Create DB.sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 21-03-19 03:50
+-- 서버 버전: 10.4.17-MariaDB
+-- PHP 버전: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `houzz`
+--
+CREATE database houzz ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
